c6a0c6f41dab3e4d24257d35d634302f5204baec
kakeru hokida <me@6mile.dev>
2024-07-01 15:28:39 +0900

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,40 +1,13 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = () => {
-  if(process.env.NODE_ENV === 'development') {
-    return {
-        reactStrictMode: true,
-        swcMinify: true,
-        images: {
-          unoptimized: true,
-        },
-        images: {
-            remotePatterns: [
-              {
-                protocol: 'https',
-                hostname: 'images.6mile.dev',
-              },
-            ],
-          },
-    }
-  }
-  
-  // Disable image delivery optimization (for CF Page)
   return {
     reactStrictMode: true,
     swcMinify: true,
     output: 'export',
     images: {
       unoptimized: true,
-    },
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'images.6mile.dev',
-          },
-        ],
-      },
+    }
   }
 }
 
