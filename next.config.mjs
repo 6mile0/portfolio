53ca01a2846a13c8d18d6d871ddbee9655ccb013
Kakeru Hokida <me@6mile.dev>
2024-12-16 11:56:05 +0900

    Revert "update npm packages"
    This reverts commit c468f17e8c6976e598e0d12ce8571faa01ab508f.

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
-/**
- * @type {import('next').NextConfig}
-*/
+/** @type {import('next').NextConfig} */
 
 const nextConfig = () => {
   return {
     reactStrictMode: true,
+    swcMinify: true,
     output: 'export',
     images: {
       unoptimized: true,
